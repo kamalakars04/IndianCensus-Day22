73ab440d5bc84a173eb18340fe29eb46ed88ef32
kamalakars04 <kamalakars04@gmail.com>
2020-10-29 16:28:23 +0530

    Add project files.

---
+++ CensusAnalyser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusDemo
+{
+    class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,US
+        }
+
+        
+    }
+}
